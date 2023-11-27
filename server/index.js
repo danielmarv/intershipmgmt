edd4572bcee7e4c876b01fe4ab1d2c95622a8e5a
Daniel Ntege <danientege785@gmail.com>
2023-11-27 18:06:13 +0300

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Internship Management System');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
