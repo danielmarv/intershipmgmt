7bb76babec54f7c054bdccfe10426aab9f88e7fc
Daniel Ntege <danientege785@gmail.com>
2023-11-28 18:44:35 +0300

    commit

---
+++ server/models/models.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const mongoose = require("mongoose");
+
+mongoose.set("debug", true);
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+    console.error("MongoDB connection error:", error);
+  });
+  
+  db.once("open", () => {
+    console.log("Connected to MongoDB!");
+});
+
+module.exports.Student = require("./students");
+module.exports.Practice = require("./practice");
+module.exports.InternshipSupervisor = require("./internshipSupervisor");
+module.exports.AdminUser = require("./adminUsers");
+module.exports.MarkSheet = require("./markSheet");
