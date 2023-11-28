86b49458011a7a364f8ca55c9aa2fda701d2298a
Daniel Ntege <danientege785@gmail.com>
2023-11-28 13:42:26 +0300

    Update internshipSupervisor.js

--- server/models/internshipSupervisor.js
+++ server/models/internshipSupervisor.js
@@ -5,6 +5,15 @@ const internshipSupervisorSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  // username: {
+  //   type: String,
+  //   required: true,
+  //   unique: true,
+  // },
+  // password: {
+  //   type: String,
+  //   required: true,
+  // },
   assignedStudents: [
     {
       type: mongoose.Schema.Types.ObjectId,
