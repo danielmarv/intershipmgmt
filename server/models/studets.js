a7e180e6e64dc2e2a1d6109eab529858771d9fb5
Daniel Ntege <danientege785@gmail.com>
2023-11-28 13:01:52 +0300

    Update studets.js

--- server/models/studets.js
+++ server/models/studets.js
@@ -1,76 +1,88 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-  },
-  campusName: {
-    type: String,
-    required: true,
-  },
-  schoolName: {
-    type: String,
-    required: true,
-  },
-  townName: {
-    type: String,
-    required: true,
-  },
-  districtName: {
-    type: String,
-    required: true,
-  },
-  distanceFromBugemaCampuses: {
-    kilometers: {
-      type: Number,
-      required: false,
+    fullName: {
+        type: String,
+        required: true,
     },
-  },
+    campusName: {
+        type: String,
+        required: true,
+    },
+    schoolName: {
+        type: String,
+        required: true,
+    },
+    townName: {
+        type: String,
+        required: true,
+    },
+    districtName: {
+        type: String,
+        required: true,
+    },
+    distanceFromBugemaCampuses: {
+        kilometers: {
+        type: Number,
+        required: false,
+        },
+    },
     schoolPractices: [
         {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Practice',
         },
     ],
-  moneyPaid: {
-    type: Number,
-    required: true,
-  },
-  studentDetails: {
-    regNo: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    currentClass: {
-      year: {
-        type: String,
-        required: true,
-      },
-      sem: {
+    moneyPaid: {
         type: Number,
         required: true,
-      },
     },
-    emailId: {
-      type: String,
-      required: true,
-      unique: true,
+    studentDetails: {
+        regNo: {
+        type: String,
+        required: true,
+        unique: true,
+        },
+        currentClass: {
+        year: {
+            type: String,
+            required: true,
+        },
+        sem: {
+            type: Number,
+            required: true,
+        },
+        },
+        emailId: {
+        type: String,
+        required: true,
+        unique: true,
+        },
+        phoneNum: {
+        type: String,
+        required: true,
+        },
     },
-    phoneNum: {
-      type: String,
-      required: true,
+    internshipCode: {
+        type: String,
+        required: true,
+        unique: true,
     },
-  },
-  internshipCode: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+    markSheet: [
+        {
+        supervisor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'InternshipSupervisor',
+            required: true,
+        },
+        marks: {
+            type: Number,
+            required: true,
+        },
+        },
+    ],
 
 });
 
 const Student = mongoose.model('Student', studentSchema);
-
 module.exports = Student;
