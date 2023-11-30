0c60eb70dd0653b1c51f0a42e573e8ea6a175ac6
Daniel Ntege <danientege785@gmail.com>
2023-11-30 20:04:24 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const dotenv = require('dotenv');
 const db = require("./models/models");
 const routes = require("./routes");
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" })); // accept data with 50mb limit
+
 
 app.use(express.json());
 
