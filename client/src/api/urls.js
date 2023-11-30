90f025cdf5f96fded14f3a2698145e875161b026
Daniel Ntege <danientege785@gmail.com>
2023-11-30 16:59:17 +0300

    commit

--- client/src/api/urls.js
+++ client/src/api/urls.js
@@ -2,10 +2,10 @@ const HOST = process.env.HOST
 
 const generateFrontendUrl = (endpoint, params = '') => {
     return `${HOST}/${endpoint}/${params}`;
-  };
+};
   
   // Example usage
-const registerUrl = generateFrontendUrl('register');
-const trackUrl = generateFrontendUrl('track', ':studentId');
-const studentAuthUrl = generateFrontendUrl('student-auth');
-const studentPracticeUrl = generateFrontendUrl('student-practice');
+export const registerUrl = generateFrontendUrl('register');
+export const trackUrl = generateFrontendUrl('track', ':studentId');
+export const studentAuthUrl = generateFrontendUrl('student-auth');
+export const studentPracticeUrl = generateFrontendUrl('student-practice');
