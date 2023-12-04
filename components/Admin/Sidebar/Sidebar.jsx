1118730e61557de9481962a530ff35a9389c8df9
Daniel Ntege <danientege785@gmail.com>
2023-12-04 15:40:24 +0300

    commit

---
+++ components/Admin/Sidebar/Sidebar.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import SidebarItems from './SidebarItems';
+
+const Sidebar = () => {
+    const sidebarItems = [
+        { icon: 'icon1', name: 'Item 1', href: '/item1', isProtected: true },
+        { icon: 'icon2', name: 'Item 2', href: '/item2', isProtected: false },
+        // Add more sidebar items as needed
+    ];
+
+    return (
+        <div className="bg-gray-800 text-white w-64 flex flex-col">
+            <div className="p-4">
+                <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+            </div>
+            <div className="flex-grow">
+                <SidebarItems items={sidebarItems} />
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
