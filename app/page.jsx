395ab2420449d1b1fb2762cf185b590e4f59be49
Daniel Ntege <danientege785@gmail.com>
2023-12-09 14:32:22 +0300

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,70 @@
-import React from 'react';
+'use client';
 
-const  Home = () => {
-  return (
-    <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center">
-            Internship Registration
-        </h1>
-    </section>
-  )
-}
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
-export default Home;
+import Form from '@components/Form';
+
+const CreateStudent = () => {
+    const router = useRouter();
+    const [submitting, setSubmitting] = useState();
+    const [formData, setFormData] = useState({
+        fullName: '',
+        campusName: '',
+        schoolName: '',
+        townName: '',
+        districtName: '',
+        kilometers: '',
+        schoolPractices: '',
+        moneyPaid: '',
+        regNo: '',
+        year: '',
+        emailId: '',
+        phoneNum: '',
+    });
+    const createStudent = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+
+        try {
+            const response = await fetch('/api/student/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    fullName: formData.fullName,
+                    campusName: formData.campusName,
+                    schoolName: formData.schoolName,
+                    townName: formData.townName,
+                    districtName: formData.districtName,
+                    kilometers: formData.kilometers,
+                    schoolPractices: formData.schoolPractices,
+                    moneyPaid: formData.moneyPaid,
+                    regNo: formData.regNo,
+                    year: formData.year,
+                    emailId: formData.emailId,
+                    phoneNum: formData.phoneNum,
+
+                })
+            })
+
+            if(response.ok) {
+                router.push('/')
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setSubmitting(false);
+        }
+    };
+
+    return (
+        <Form 
+            type="Create"
+            formData={formData}
+            setFormData={setFormData}
+            submitting={submitting}
+            handleSubmit={createStudent}
+        />
+    )
+};
+
+export default CreateStudent;
