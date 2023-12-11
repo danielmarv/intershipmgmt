a91a3eea51a12c7678041f75740e93f03a1c2036
Daniel Ntege <danientege785@gmail.com>
2023-12-12 00:01:59 +0300

    commit

--- components/Sidebar/SidebarItem.jsx
+++ components/Sidebar/SidebarItem.jsx
@@ -1,19 +1,25 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-const SideBarItem = ({ Icon, label, dropdown, navPath, children, toggleMethod, toggleState }) => {
+const SideBarItem = ({ Icon, label, dropdown, navPath, toggleMethod, toggleState }) => {
   const router = useRouter();
-  // get current route
   const currentRoute = router.pathname;
-  // check if current route contains navPath
   const isCurrentRoute = currentRoute.includes(navPath);
+
+  const handleClick = () => {
+    // Call the toggleMethod if it's provided
+    if (toggleMethod) {
+      toggleMethod(!toggleState); // Toggle the state
+    }
+  };
 
   return (
     <div
-      className={`cursor-pointer ${toggleState && 'bg-sidebar-blue rounded'}`}
       role='button'
       tabIndex={0}
-      onClick={dropdown && toggleMethod}>
+      onClick={handleClick}
+      onKeyDown={(e) => e.key === 'Enter' && handleClick()}
+    >
       <Link href={navPath || '#'}>
         <div className={`flex items-center justify-between w-full h-12 hover:cursor-pointer mt-2`}>
           <div className={`flex items-center w-full`}>
@@ -25,11 +31,13 @@ const SideBarItem = ({ Icon, label, dropdown, navPath, children, toggleMethod, t
             <div
               className={`flex items-center py-3 px-4 w-full ${
                 isCurrentRoute && 'bg-primary-50 rounded'
-              }`}>
+              }`}
+            >
               <div
                 className={`w-8 h-8 rounded-full flex items-center justify-center mr-4 ${
                   isCurrentRoute && 'text-blue-600'
-                }`}>
+                }`}
+              >
                 <Icon fill={isCurrentRoute ? '#145FFF' : '#6F87A1'} />
               </div>
 
@@ -38,7 +46,8 @@ const SideBarItem = ({ Icon, label, dropdown, navPath, children, toggleMethod, t
                   isCurrentRoute
                     ? 'text-blue-600 mr-3'
                     : 'font-normal text-secondary-neutral-light-800'
-                }`}>
+                }`}
+              >
                 {label}
               </h3>
             </div>
