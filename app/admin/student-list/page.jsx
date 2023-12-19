1f074a89f7f354b47d353d224743c362bf6a9347
Daniel Ntege <danientege785@gmail.com>
2023-12-19 12:58:38 +0300

    Update page.jsx

--- app/admin/student-list/page.jsx
+++ app/admin/student-list/page.jsx
@@ -23,7 +23,6 @@ const Students = () => {
             'Content-Type': 'application/json',
             'Cache-Control': 'no-cache, no-store, must-revalidate',
             'Pragma': 'no-cache',
-            'Expires': '0',
           },
         });
         
