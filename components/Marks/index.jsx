ee45e75585e859e29e6c9dc171ece1b02f0eac51
Daniel Ntege <danientege785@gmail.com>
2024-01-05 15:30:01 +0300

    Update index.jsx

--- components/Marks/index.jsx
+++ components/Marks/index.jsx
@@ -1,43 +1,16 @@
-// MarkSheetForm.js
-
 import { useState, useEffect } from 'react';
 import Select from 'react-select';
 
-const MarkSheetForm = ({ students, supervisors, onSubmit, handleSubmit }) => {
-  const [selectedStudent, setSelectedStudent] = useState('');
-  const [selectedSupervisor, setSelectedSupervisor] = useState('');
-  const [marks, setMarks] = useState('');
-
-  const handleStudentChange = (selectedOption) => {
-    setSelectedStudent(selectedOption);
-  };
-
-  const handleSupervisorChange = (selectedOption) => {
-    setSelectedSupervisor(selectedOption);
-  };
-
-  const handleMarksChange = (e) => {
-    setMarks(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Pass the selected values to the parent component for submission
-    onSubmit({
-      student: selectedStudent,
-      supervisor: selectedSupervisor,
-      marks: marks,
-    });
-  };
+const MarkSheetForm = ({ formData, setFormData, students, supervisors, onSubmit, handleSubmit }) => {
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>Select Student:</label>
         <Select
-          value={selectedStudent}
-          onChange={handleStudentChange}
+          value={formData.student}
+          onChange={(e) => setFormData({ ...formData,
+          student: e.target.value })}
           options={students.map((student) => ({ value: student._id, label: student.fullName }))}
           isSearchable
           placeholder="Search for a student..."
@@ -46,8 +19,9 @@ const MarkSheetForm = ({ students, supervisors, onSubmit, handleSubmit }) => {
       <div>
         <label>Select Supervisor in the same district:</label>
         <Select
-          value={selectedSupervisor}
-          onChange={handleSupervisorChange}
+          value={formData.supervisor}
+          onChange={(e) => setFormData({ ...formData,
+          supervisor: e.target.value })}
           options={supervisors.map((supervisor) => ({ value: supervisor._id, label: supervisor.fullName }))}
           isSearchable
           placeholder="Search for a supervisor..."
@@ -55,7 +29,12 @@ const MarkSheetForm = ({ students, supervisors, onSubmit, handleSubmit }) => {
       </div>
       <div>
         <label>Marks:</label>
-        <input type="number" value={marks} onChange={handleMarksChange} />
+        <input 
+          type="number" 
+          value={formData.marks}
+          onChange={(e) => setFormData({ ...formData,
+          marks: e.target.value })}
+          />
       </div>
       <div>
         <button type="submit">Submit</button>
